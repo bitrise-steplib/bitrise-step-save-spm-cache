3adaab1eeba70584ceed2b790cab66a570c76a1d
Olivér Falvai <oliver.falvai@bitrise.io>
2023-04-13 16:03:03 +0200

    Fix Content-Length header in cache upload request (#5)

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/bitrise-io/go-utils/v2/exitcode"
 	"github.com/bitrise-io/go-utils/v2/log"
 	"github.com/bitrise-io/go-utils/v2/pathutil"
-	"github.com/bitrise-steplib/steps-save-spm-cache/step"
+	"github.com/bitrise-steplib/bitrise-step-save-spm-cache/step"
 )
 
 func main() {
